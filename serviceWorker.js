05c153eb671d50822735944bf429af45dc517d27
Daniel <daniel_meisler@web.de>
2023-03-15 17:25:47 +0100

    feat: add css to serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,8 +2,10 @@ const staticDiceCup = "dev-dice-cup-v1"
 const assets = [
   "/",
   "DiceCup.html",
-  "DiceCup.css",
-  "Script/Build/Script.js",
+  "Game/Styling/DiceCup.css",
+  "Game/Styling/Menu.css",
+  "Game/Styling/Game.css",
+  "Game/Script/Build/Script.js",
 ]
 
 self.addEventListener("install", installEvent => {
