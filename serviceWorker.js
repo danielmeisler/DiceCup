cfbd82ea83875c40d4ab31c6ea9eb3c8dc77f390
Daniel Meisler <daniel_meisler@web.de>
2023-04-19 18:11:03 +0200

    test

--- serviceWorker.js
+++ serviceWorker.js
@@ -20,18 +20,10 @@ self.addEventListener("install", installEvent => {
   )
 })
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-      caches.open(cacheName)
-          .then(function(cache) {
-              cache.match(event.request)
-                  .then( function(cacheResponse) {
-                      fetch(event.request)
-                          .then(function(networkResponse) {
-                              cache.put(event.request, networkResponse)
-                          })
-                      return cacheResponse || networkResponse
-                  })
-          })
+      fetch(event.request).catch(function() {
+          return caches.match(event.request)
+      })
   )
-});
+})
